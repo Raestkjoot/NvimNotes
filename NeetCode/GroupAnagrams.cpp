17da1a993397357ba10caa52fa858279d778fa00
Raestkjoot <gunnarmagnum@gmail.com>
2026-05-16 15:56:16 +0200

    group anagrams + config so far

--- NeetCode/GroupAnagrams.cpp
+++ NeetCode/GroupAnagrams.cpp
@@ -1,9 +1,28 @@
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <unordered_map>
 
 class Solution {
-	std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs)
+public:
+	std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs)
 	{
-		
+		std::unordered_map<std::string, std::vector<std::string>> stringMap;
+
+		for (const std::string& str : strs)
+		{
+			std::string sortedStr = str;
+			sort(sortedStr.begin(), sortedStr.end());
+			stringMap[sortedStr].push_back(str);
+		}
+
+		std::vector<std::vector<std::string>> retval;
+
+		for (auto& pair : stringMap)
+		{
+			retval.push_back(pair.second);
+		}
+
+		return retval;
 	}
 };
---
+++ init.lua
@@ -0,0 +1,6 @@
+vim.opt.relativenumber = true
+vim.opt.tabstop = 3
+vim.opt.shiftwidth = 0 -- when set to 0 this will automatically match tabstop
+vim.opt.expandtab = false
+vim.opt.breakindent = false
+vim.opt.cindent = true
